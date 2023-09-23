4b642dcb8487d3fb8fbe4150188829f8837be8af
Fad <fadhilrayhan3@gmail.com>
2023-09-23 16:43:11 +0700

    membuat website

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const navbarNav = document.querySelector('.navbar-nav');
+
+
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+//Klik di luar sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e){
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+
